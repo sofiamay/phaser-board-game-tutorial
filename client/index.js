6d8f7daa4cc1d5cf44c6e7d4a91e29bbefe164b8
Sofia Berlin <sofiamberlin@gmail.com>
2022-10-31 19:39:54 -0700

    Add Card helper and card images. Load cards when clicking on deal cards

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import * as Phaser from 'phaser';
-import PlayScene from './src/scenes/PlayScene';
+import Game from './src/scenes/game';
+
 
 const config = {
   name: 'app',
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: [PlayScene],
+  width: 1200,
+  height: 780,
+  scene: [Game],
 };
 
 window.game = new Phaser.Game(config);
---
+++ client/src/scenes/game.js
@@ -0,0 +1,55 @@
+import Card from '../helpers/card';
+
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Game'
+        });
+    }
+
+    preload() {
+        this.load.image('cyanCardFront', 'src/static/CyanCardFront.png');
+        this.load.image('cyanCardBack', 'src/static/CyanCardBack.png');
+        this.load.image('magentaCardFront', 'src/static/MagentaCardFront.png');
+        this.load.image('magentaCardBack', 'src/static/MagentaCardBack.png');
+    }
+
+    create() {
+        this.dealText = this.add.text(75, 350, ['DEAL CARDS'])
+        	.setFontSize(18).setFontFamily('Trebuchet MS')
+        	.setColor('#00ffff')
+        	.setInteractive();
+
+        let self = this;
+
+        this.dealCards = () => {
+        	for (let i = 0; i < 5; i++) {
+                let playerCard = new Card(this);
+                playerCard.render(475 + (i * 100), 650, 'cyanCardFront');
+            }
+        }
+
+        this.dealText.on('pointerdown', function () {
+            self.dealCards();
+        })
+
+        this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#ff69b4');
+        })
+
+        this.dealText.on('pointerout', function () {
+            self.dealText.setColor('#00ffff');
+        })
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
+
+
+    }
+    
+    update() {
+    
+    }
+}
