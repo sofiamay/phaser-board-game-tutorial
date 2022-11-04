06e05303ac3ce36cedeecc5c5f784c9ebf25849f
Sofia Berlin <sofiamberlin@gmail.com>
2022-11-04 12:25:08 -0700

    Add CORS heading to server socket.io and instal socket.io on client

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,3 +1,5 @@
+import io from 'socket.io-client';
+
 import Card from '../helpers/card';
 import Zone from '../helpers/zone';
 
@@ -16,6 +18,12 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
+        this.socket = io('http://localhost:3000');
+
+        this.socket.on('connect', function() {
+            console.log('Connected!');
+        });
+
         this.zone = new Zone(this);
         this.dropZone = this.zone.renderZone();
         this.outline = this.zone.renderOutline(this.dropZone);
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const server = require('express')();
 const http = require('http').createServer(server);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+	cors: {
+		origin: "http://localhost:8080",
+		methods: ["GET", "POST"]
+	}
+});
 
 io.on('connection', function(socket) {
 	console.log('A user connected: ' + socket.id);
