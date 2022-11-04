e52a4539b9abdc0842a78ec736ee0c907b488e13
Sofia Berlin <sofiamberlin@gmail.com>
2022-11-04 12:36:29 -0700

    Finish server

--- server.js
+++ server.js
@@ -7,8 +7,24 @@ const io = require('socket.io')(http, {
 	}
 });
 
+let players = [];
+
 io.on('connection', function(socket) {
 	console.log('A user connected: ' + socket.id);
+	players.push(socket.id);
+
+	if (players.length === 1) {
+		io.emit('isPlayerA');
+	};
+
+	socket.on('dealCards', function() {
+		io.emit('dealCards');
+	});
+
+	socket.on('cardPlayed', function(gameObject, isPlayerA) {
+		io.emit('cardPlayed', gameObject, isPlayerA);
+	});
+
 
 	socket.on('disconnect', function() {
 		console.log('A user disconnected: ' + socket.id);
