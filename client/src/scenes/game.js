96d5c41eb7d9c0abe3f1273303e46f49337d0eb5
Sofia Berlin <sofiamberlin@gmail.com>
2022-11-15 19:14:57 -0800

    Client responds to opponents card being played by rendering card in play zone

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -50,6 +50,27 @@ export default class Game extends Phaser.Scene {
             gameObject.y = dragY;
         })
 
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff69b4);
+            self.children.bringToTop(gameObject);
+        })
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+            gameObject.setTint();
+            if (!dropped) {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+        })
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+            dropZone.data.values.cards++;
+            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+            gameObject.y = dropZone.y;
+            gameObject.disableInteractive();
+            self.socket.emit('cardPlayed', gameObject, self.isPlayerA);
+        })
+
         // SOCKET FUNCTIONALITY
         this.socket = io('http://localhost:3000');
 
@@ -64,6 +85,16 @@ export default class Game extends Phaser.Scene {
         this.socket.on('dealCards', function() {
             self.dealer.dealCards();
             self.dealText.disableInteractive;
+        });
+
+        this.socket.on('cardPlayed', function(gameObject, isPlayerA) {
+            if (isPlayerA !== self.isPlayerA) {
+                let sprite = gameObject.textureKey;
+                self.opponentCards.shift().destroy();
+                self.dropZone.data.values.cards++;
+                let card = new Card(self);
+                card.render(((self.dropZone.x - 350) + (self.dropZone.data.values.cards * 50)), (self.dropZone.y), sprite).disableInteractive();
+            }
         });
 
 
